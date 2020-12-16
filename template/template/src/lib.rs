e0e9e9119e208843e6499f4a777aa4d514427790
Jacob <jacob@notactivelylooking.com>
2020-12-16 23:36:43 +0000

    Adding docs.

--- template/template/src/lib.rs
+++ template/template/src/lib.rs
@@ -1,11 +1,13 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+/// the docs is in a comment above the pyfunction!
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 	Ok((a + b).to_string())
 }
 
+/// the docs is in a comment above the pymodule!
 #[pymodule]
 fn template(_py: Python, m: &PyModule) -> PyResult<()> {
 	m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
